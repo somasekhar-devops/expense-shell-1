5f93997b40d9425f70797a88ce7a898a21421b25
somasekhar-devops <somasekhargoudm@gmail.com>
2024-05-01 15:47:45 +0530

    expense project - calling scripts from other script

--- frontend.sh
+++ frontend.sh
@@ -5,28 +5,28 @@ source ./common.sh
 check_root
 
 dnf install nginx -y &>>$Logfile
-Validate $? "Installing nginx"
+#Validate $? "Installing nginx"
 
 systemctl enable nginx &>>$Logfile
-Validate $? "Enabling nginx"
+#Validate $? "Enabling nginx"
 
 systemctl start nginx &>>$Logfile
-Validate $? "Starting nginx"
+#Validate $? "Starting nginx"
 
 rm -rf /usr/share/nginx/html/* &>>$Logfile
-Validate $? "Removing existing files from the directory"
+#Validate $? "Removing existing files from the directory"
 
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$Logfile
-Validate $? "Downloading the fronend application"
+#Validate $? "Downloading the fronend application"
 
 cd /usr/share/nginx/html &>>$Logfile
-Validate $? "Moving to nginx directory"
+#Validate $? "Moving to nginx directory"
 
 unzip /tmp/frontend.zip &>>$Logfile
-Validate $? "Extracting the frontend code"
+#Validate $? "Extracting the frontend code"
 
 cp /home/ec2-user/expenses-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$Logfile
-Validate $? "Copying frontend config file"
+#Validate $? "Copying frontend config file"
 
 systemctl restart nginx &>>$Logfile
-Validate $? "Restarting nginx"
+#Validate $? "Restarting nginx"
