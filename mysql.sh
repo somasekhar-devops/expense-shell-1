7f8eda8b002ed3b088e7ed4724a450c2444c7900
somasekhar-devops <somasekhargoudm@gmail.com>
2024-05-01 15:24:54 +0530

    expense project - calling scripts from other script

--- mysql.sh
+++ mysql.sh
@@ -16,10 +16,10 @@ systemctl enable mysqld &>>$Logfile
 systemctl start mysqld &>>$Logfile
 # Validate $? "Starting mysql server"
 
-mysql -h db.sekhardevops.online -uroot -p${mysql_root_password} -e 'SHOW DATABASES;' &>>$Logfile
+mysql -h db.sekhardevops.online -uroot -pExpenseApp@1 -e 'SHOW DATABASES;' &>>$Logfile
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$Logfile
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$Logfile
     # Validate $? "Setting up the root password for mysql server"
 else
     echo -e "Mysql passowrd setup already done ...$Y Skipping..$N"
